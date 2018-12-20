23b05cb27d918abf7dddde1ab503f3636a565b5d
richardsunny2015 <richardsunny2015@gmail.com>
2018-12-20 15:52:22 -0500

    chore(store): Add values reducer

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,9 @@ import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import values from './values'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({user, values})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
