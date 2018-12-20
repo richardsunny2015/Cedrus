76f90a8792ab328c5a5215a9e4bef23471e336b5
richardsunny2015 <richardsunny2015@gmail.com>
2018-12-20 14:50:55 -0500

    fix(arithmetic): Change route from a get to a post

--- server/arithmetic/index.js
+++ server/arithmetic/index.js
@@ -2,8 +2,8 @@ const router = require('express').Router()
 const {calculate} = require('../utilities')
 module.exports = router
 
-router.get('/', (req, res, next) => {
-    // const {calculation, numbers} = req.body
+router.post('/', (req, res, next) => {
+    const {calculation, numbers} = req.body
 
-    res.status(200).json(calculate('add', [1, 2]))
+    res.status(201).json(calculate('add', [1, 2]))
 })
