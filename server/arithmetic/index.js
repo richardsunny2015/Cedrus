899e552dc40041ee5eb53a535adc81b650556d07
richardsunny2015 <richardsunny2015@gmail.com>
2018-12-20 16:09:57 -0500

    fix(arithmetic): Use req.body instead of hard-coded values

--- server/arithmetic/index.js
+++ server/arithmetic/index.js
@@ -5,5 +5,5 @@ module.exports = router
 router.post('/', (req, res, next) => {
     const {calculation, numbers} = req.body
 
-    res.status(201).json(calculate('add', [1, 2]))
+    res.status(201).json(calculate(calculation, numbers))
 })
