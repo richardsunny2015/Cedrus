2c84f83c25d03ea772790e767aafafc09f0e60f2
richardsunny2015 <richardsunny2015@gmail.com>
2018-12-20 15:18:54 -0500

    fix(calculateForm): Integrate onChange handlers for numbers

--- client/components/calculateForm.js
+++ client/components/calculateForm.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 
+const calculations = ['add', 'subtract', 'divide', 'multiply']
+
 export default class CalculateForm extends Component {
   constructor(props) {
     super(props)
@@ -9,18 +11,33 @@ export default class CalculateForm extends Component {
       secondNum: 0
     } //
   }
-
+  changeNumberHandler = firstOrSecond => evt => {
+    // Return a function that would change state based on firstOrSecond
+    console.log(evt.target.value)
+    this.setState({[firstOrSecond]: evt.target.value})
+  }
   render() {
     return (
       <div>
         <label htmlFor="firstNum">
           <small>First Number: </small>
         </label>
-        <input name="firstNum" type="number" />
+        <input
+          name="firstNum"
+          type="number"
+          value={this.state.firstNum}
+          onChange={this.changeNumberHandler('firstNum')}
+        />
         <label htmlFor="secondNum">
           <small>Second Number: </small>
         </label>
-        <input name="secondNum" type="number" />
+        <input
+          name="secondNum"
+          type="number"
+          value={this.state.secondNum}
+          onChange={this.changeNumberHandler('secondNum')}
+        />
+
       </div>
     )
   }
