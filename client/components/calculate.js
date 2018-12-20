88f689cd13b9c8ccc9f00c406e867ba2d1632522
richardsunny2015 <richardsunny2015@gmail.com>
2018-12-20 15:38:20 -0500

    refactor(calculate): Create calculate component that renders form

---
+++ client/components/calculate.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CalculateForm from './calculateForm'
+
+
+const Calculate = props => {
+    return (
+        <div>
+            <CalculateForm />
+        </div>
+    )
+}
+
+export default Calculate
